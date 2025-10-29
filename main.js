3adb25ae1163b647ae2c741a4ecb025bc999f3dc
mileninha <vitoria.ribeiro.milena@escola.pr.gov.br>
2025-10-29 11:44:50 -0300

    j

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log(avanca)
 avanca.forEach =>{
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo')
-        cons proximoPasso = 'passo-' + getAttribute('data-proximo')
+        cons proximoPasso = 'passo-' + this.getAttribute('data-proximo')
     atual.classList.remove('ativo')
     
     document.getElementById(proximoPasso).classList.add('ativo')
