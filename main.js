c49012dee6407fe81619b4c96d8e6a2a28a1f34b
mileninha <vitoria.ribeiro.milena@escola.pr.gov.br>
2025-10-29 07:58:30 -0300

    j

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+console.log(avanca)
+
+avanca.forEach =>{
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        cons proximoPasso = 'passo-' + getAttribute('data-proximo')
+    atual.classList.remove('ativo')
+    
+    document.getElementById(proximoPasso).classList.add('ativo')
+    })
+}
 
